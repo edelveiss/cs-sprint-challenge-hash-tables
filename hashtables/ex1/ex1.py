395e5e72cb698ad625fd70ec668548610757f447
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-11-06 18:33:01 -0800

    ex1.py is completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # dict_weights = {print(f"{x}:{i}") for i, x in enumerate(weights)}
+    dict_weights = {}
+
+    for i in range(len(weights)):
+        if limit - weights[i] in dict_weights:
+            return (i,dict_weights[limit - weights[i]] )
+        else:
+            dict_weights[weights[i]] = i
 
     return None
+
+weights = [ 4, 6, 10, 15, 16 ]
+length = 5
+limit = 21
+print(get_indices_of_item_weights(weights, length, limit))
+
+
